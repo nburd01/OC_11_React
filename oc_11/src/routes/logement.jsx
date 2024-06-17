2cd3d102bb50a9bd493ba4f829b0e22589daddf3
nburd01 <n.burdon@protonmail.com>
2024-06-17 17:13:05 +0200

    all elements appear on logement page, now need to solve css

--- oc_11/src/routes/logement.jsx
+++ oc_11/src/routes/logement.jsx
@@ -22,28 +22,59 @@ export default function Logement() {
 
   return (
     <div id="logements">
-      <div className="logement_banner" style={backgroundImageStyle}>
-        <h2>{logements.title}</h2>
-      </div>
-      <div className="title">{logements.title}</div>
-      <div className="tags">
-        {logements.tags.map((tag, index) => (
-          <div key={index}>{tag}</div>
-        ))}
-      </div>
-      <div className="nameAndLocal"></div>
-      <div className="host">{logements.host.name}</div>
-      <img src={logements.host.picture} className="host_pic" alt="host" />
+      <div className="logement_banner" style={backgroundImageStyle}></div>
+      <div className="logements-info">
+        <div className="part1">
+          <div className="title">
+            <h1>{logements.title}</h1>
+          </div>
+          <div className="location">{logements.location}</div>
+          <div className="tags">
+            {logements.tags.map((tag, index) => (
+              <div key={index} className="tag">
+                {tag}
+              </div>
+            ))}
+          </div>
+          <div className="nameAndLocal"></div>
+        </div>
+        <div className="part2">
+          <div className="host">
+            <div className="host_name">{logements.host.name}</div>
+            <img src={logements.host.picture} className="host_pic" alt="host" />
+          </div>
 
-      <div className="rating">
-        {Array.from({ length: logements.rating }).map((_, index) => (
-          <img key={index} src={star} alt="Star" className="star-icon" />
-        ))}
-        {Array.from({ length: 5 - logements.rating }).map((inactive, index) => (
-          <img key={index} src={inactive_star} alt="" className="star-icon" />
-        ))}
+          <div className="rating">
+            {Array.from({ length: logements.rating }).map((_, index) => (
+              <img key={index} src={star} alt="Star" className="star-icon" />
+            ))}
+            {Array.from({ length: 5 - logements.rating }).map(
+              (inactive, index) => (
+                <img
+                  key={index}
+                  src={inactive_star}
+                  alt=""
+                  className="star-icon"
+                />
+              )
+            )}
+          </div>
+        </div>
       </div>
-      <div className="dropdowns"></div>
+      <div className="dropdowns">
+        <div class="dropdown">
+          <span>Description</span>
+          <div class="dropdown-content">{logements.description}</div>
+        </div>
+        <div className="dropdown">
+          <span>Equipements</span>
+          <div className="dropdown-content">
+            {logements.equipments.map((equipement, index) => (
+              <p key={index}>{equipement}</p>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
